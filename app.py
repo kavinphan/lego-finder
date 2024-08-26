f7a1dbd6df516d2a0ccf20639fc8abfba37b0427
kphan32 <kavin@kphan.tech>
2024-08-26 19:16:41 -0400

    Add scrapers file

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
 from rebrickable import get_sets, get_set
+from scrapers import scrape_amazon, scrape_ebay
 
 app = Flask(__name__)
 
 load_dotenv()
 
+# Landing page with the search input
 @app.route("/", methods=['GET'])
 def home():
     return render_template('home.html')
 
+# Search results page with links to sets
 @app.route("/search", methods=['POST'])
 def search():
     search = request.form['search']
@@ -18,8 +21,11 @@ def search():
 
     return render_template('search.html', search=search, results=results)
 
+# Set page to show a lego set and the scraped information
 @app.route("/set/<id>", methods=['GET'])
 def set(id):
     set = get_set(id)
+    amazon_results = scrape_amazon(id)
+    ebay_results = scrape_ebay(id)
 
-    return render_template('set.html', set=set)
+    return render_template('set.html', set=set, amazon_results=amazon_results, ebay_results=ebay_results)
---
+++ scrapers.py
@@ -0,0 +1,15 @@
+def scrape_amazon(set_id):
+    # TODO scrape amazon
+
+    return {
+        "price": "$59.99",
+        "in_stock": False
+    }
+
+def scrape_ebay(set_id):
+    # TODO scrape ebay
+
+    return {
+        "price": "$29.99",
+        "in_stock": True
+    }
